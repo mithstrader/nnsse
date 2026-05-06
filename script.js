9190cdd3ed17421204c028895c6c3b4219b32a79
mithstrader <miths.trader@gmail.com>
2026-05-06 11:43:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -959,9 +959,9 @@ async function fetchAllData() {
     console.log('Starting data fetch...');
 
     // 0. OI Gainers Heatmap
-   // fetch('https://smartoptions.trendlyne.com/phoenix/api/fno/market/filter/?mtype=futures&expDate=2026-05-26&screenType=oi-gainers')
+   fetch('https://smartoptions.trendlyne.com/phoenix/api/fno/market/filter/?mtype=futures&expDate=2026-05-26&screenType=oi-gainers')
         //fetch('/trendlyne/phoenix/api/fno/market/filter/?mtype=futures&expDate=2026-05-26&screenType=oi-gainers')
-      fetch('https://api.codetabs.com/v1/proxy?quest=' + encodeURIComponent('https://smartoptions.trendlyne.com/phoenix/api/fno/market/filter/?mtype=futures&expDate=2026-05-26&screenType=oi-gainers'))
+      // fetch('https://api.codetabs.com/v1/proxy?quest=' + encodeURIComponent('https://smartoptions.trendlyne.com/phoenix/api/fno/market/filter/?mtype=futures&expDate=2026-05-26&screenType=oi-gainers'))
         .then(res => { if (!res.ok) throw new Error('OI Gainers Network Error'); return res.json(); })
         .then(data => {
             if (data && data.body && data.body.tableData) {
